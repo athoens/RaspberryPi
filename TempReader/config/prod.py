0dc71c91e16412e2bbcf4386e0443ceb8da0ecb9
c.thoens <christoph.thoens@gmail.com>
2016-03-05 14:15:51 +0100

    Added in memory history

--- TempReader/config/prod.py
+++ TempReader/config/prod.py
@@ -33,3 +33,5 @@ slavePort = 8090
 openWeatherMapApiKey = os.environ.get('OPEN_WEATHER_MAP_API_KEY')
 openWeatherMapLat = os.environ.get('OPEN_WEATHER_MAP_LAT')
 openWeatherMapLong = os.environ.get('OPEN_WEATHER_MAP_LONG')
+collectInterval = 60
+maxHistoryLength = 6000
